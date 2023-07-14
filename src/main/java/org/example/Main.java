df2a406d89742fc3d7fbccf21eadbe49800d3117
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-07-14 14:27:45 -0300

    :sparkles: feat: criado classe conexao

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
 
+import static org.example.connection.Conexao.getConnection;
+
 public class Main {
     public static void main(String[] args) {
+        getConnection();
 
     }
 }
